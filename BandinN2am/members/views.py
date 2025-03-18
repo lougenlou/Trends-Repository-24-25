1946a2dcfacb87eb0fad432d276763736407ac39
lougenlou <168263256+lougenlou@users.noreply.github.com>
2025-03-17 20:44:37 -0700

    Add files via upload
    fr fr

--- BandinN2am/members/views.py
+++ BandinN2am/members/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
 
 # Create your views here.
